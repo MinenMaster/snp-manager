23910c36fb7b7d53243b1a76bdf12fe0d84edb34
Dominik Meister <dominik.s.meister@gmail.com>
2025-02-27 15:38:30 +0100

    fix: replace 'throw new Error' with a NextResponse

--- app/api/tools/authenticateJWT.ts
+++ app/api/tools/authenticateJWT.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
@@ -11,13 +11,19 @@ export const authenticateJWT = (req: NextRequest) => {
 
     const token = authHeader.split(" ")[1];
     if (!token) {
-        throw new Error("Unauthorized");
+        return new NextResponse("Unauthorized", {
+            status: 401,
+            headers: { "Content-Type": "application/json" },
+        });
     }
 
     try {
         const user = jwt.verify(token, JWT_SECRET);
         return user;
     } catch {
-        throw new Error("Forbidden");
+        return new NextResponse("Forbidden", {
+            status: 403,
+            headers: { "Content-Type": "application/json" },
+        });
     }
 };
